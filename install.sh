612b3a0030deb2baea8a0ddc348bbb094450cbb2
gsistelos <gsistelos@proton.me>
2026-04-04 11:23:27 -0300

    feat: add -P to pwd

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 TARGET_CLI_DIR="${1:-$HOME/.local/bin}"
 TARGET_OPENVPN3_SETUP_CLI="$TARGET_DIR/openvpn3-setup"
